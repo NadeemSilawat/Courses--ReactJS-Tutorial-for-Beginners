c373d855e8ec006e70add851523526828c21a334
NadeemSilawat <im.nadeem.silawat@gmail.com>
2025-04-22 23:30:08 +0530

    Inline Js in React

---
+++ hello/src/component/Inline.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const heading = {
+    fontSize: '72px',
+    color: 'blue'
+}
+
+function Inline() {
+  return (
+    <div className={heading}>Inline</div>
+  )
+}
+
+export default Inline
